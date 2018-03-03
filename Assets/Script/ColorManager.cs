c548a4d200fe642af0cc94bb796e769076a06f1f
Unknown <nkc.keita.takahashi@gmail.com>
2018-03-03 22:33:16 +0900

    color manager

---
+++ Assets/Script/ColorManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ColorManager : MonoBehaviour {
+    public Color[] Color;
+    public int i;
+    GameObject camera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camera = GameObject.Find("Main Camera");
+        camera.GetComponent<Camera>().backgroundColor = Color[i];
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ Assets/Script/text.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class text : MonoBehaviour {
+    public Color textcolor;
+    public bool Alphaswitch;
+    public float outtime;
+    public float intime;
+    float fadetime;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        plzanykey();
+    }
+    void plzanykey()
+    {
+        if (Alphaswitch)
+        {
+            gameObject.GetComponent<Text>().color = new Color(1, 1, 1, Mathf.Lerp(0, 1, fadetime /intime));
+        }
+        else
+        {
+            gameObject.GetComponent<Text>().color = new Color(1, 1, 1, Mathf.Lerp(1,0, fadetime/outtime));
+        }
+        if (gameObject.GetComponent<Text>().color.a == 1)
+        {
+            Alphaswitch = false;fadetime = 0;
+        }
+        else if(gameObject.GetComponent<Text>().color.a==0)
+        {
+            Alphaswitch = true;fadetime = 0;
+        }
+        fadetime += Time.deltaTime;
+    }
+}
